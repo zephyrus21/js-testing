0a9d40e1fe8940a8d10a2dccdea6c82071459e7f
zephyrus21 <pandey.piyus@gmail.com>
2021-04-26 12:31:02 +0530

    integration test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('Piyush', 21);
+    expect(text).toBe('Piyush (21 years old)');
+});
+
+test('should generate valid text output', () => {
+    const text = checkAndGenerate('Piyush', 21);
     expect(text).toBe('Piyush (21 years old)');
 });
