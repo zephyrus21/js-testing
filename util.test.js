e44da0c6146986bc2734ce57203205aada9060ac
zephyrus21 <pandey.piyus@gmail.com>
2021-04-26 13:20:05 +0530

    adding puppeteer

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
@@ -8,4 +9,21 @@ test('should output name and age', () => {
 test('should generate valid text output', () => {
     const text = checkAndGenerate('Piyush', 21);
     expect(text).toBe('Piyush (21 years old)');
+});
+
+test('should click around', async() => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920,1080'],
+    });
+    const page = await browser.newPage();
+
+    await page.goto('file:///E:/Projects/JavaScript/js-testing/index.html');
+
+    await page.click('input#name');
+    await page.type('input#name', 'Piyush');
+    await page.click('input#age');
+    await page.type('input#age', '21');
+    await page.click('#btnAddUser');
 });
